f18559addaf16760ca882c13a515c10c6889e741
Juan Orellana <juan.orellana2@catolica.edu.sv>
2025-04-25 19:06:43 -0600

    Agregar archivos de proyecto.

---
+++ Models/HelpDeskFlowContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HelpDeskFlow.Models
+{
+    public class HelpDeskFlowContext : DbContext
+    {
+        public HelpDeskFlowContext(DbContextOptions<HelpDeskFlowContext> options) : base(options)
+        {
+
+        }
+    }
+}
